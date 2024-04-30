608ff82262d3db6b640b77fb4e9afeaa8acbad89
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-04-30 23:08:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,125 @@
-# Leagal-Document-Assistant
+# Legal-Documentation-Assistant
+## Problem Statement
+
+Legal documentation can be a complicated and time-consuming process, especially for individuals and small businesses who may not have access to legal resources. In addition, the language and jargon used in legal documents can be difficult for non-lawyers to understand, which can lead to errors and misunderstandings. 
+
+**Objective**: The objective of this hackathon challenge is to develop an AI-powered solution that can simplify legal documentation for individuals and small businesses in India, by automatically drafting legal documents in plain language and using easy-to-understand terms. 
+
+**Potential Features**: 
+
+1. User-friendly interface for inputting relevant information such as parties involved, terms of the agreement, and other necessary details. 
+
+2. AI-powered document generation that automatically drafts legal documents in plain language and using easy-to-understand terms. 
+
+3. Ability to customize legal documents based on the specific needs of the user. 
+
+4. Integration with existing legal resources and databases to ensure accuracy and completeness of the legal documents. 
+
+5. Option for users to seek legal advice from an expert in case of complex legal issues. 
+
+**Impact**: The proposed solution can greatly benefit individuals and small businesses in India, who often face challenges with legal documentation due to limited access to legal resources. By simplifying legal documentation, this solution can potentially save time, reduce errors, and increase access to justice. 
+
+**Data**: Participants can use publicly available legal databases and resources to train the AI model for document generation. 
+
+**Deliverables**: 
+
+1. A working prototype of the AI-powered legal documentation assistant, demonstrating its functionality and ease of use. 
+
+2. A presentation outlining the features and potential impact of the solution, as well as its technical architecture and data requirements. 
+
+3. Code and documentation for the solution, along with instructions for deployment and maintenance. 
+
+## Tech stack used
+
+- ![react](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)
+
+- ![tailwind](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)
+
+- ![python](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue)
+
+- ![flask](https://img.shields.io/badge/flask-%23000.svg?style=for-the-badge&logo=flask&logoColor=white)
+
+- ![mysql](https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white)
+
+## Setup
+
+- First, clone the repository into your system, and enter the repository in your terminal
+
+```
+git clone https://github.com/PritK99/Legal-Documentation-Assistant.git
+cd Legal-Documentation-Assistant/
+```
+
+- To start the client, run these commands
+
+```
+cd client/src/
+
+npm i
+
+npm run start
+```
+
+- To start the server, run these commands on a new terminal, at folder `Legal-Documentation-Server/`
+```
+cd server/
+
+pip install flask cors python-docx requests MySQLdb mammoth
+
+python3 app.py
+```
+
+## Screenshots of the Webpage
+
+- #### Home Page
+
+    ![home](./assets/image.png)
+
+- #### Documents Page
+
+    ![forms](./assets/image-1.png)
+
+- #### Dynamic form page (generated for the specific legal document)
+
+    ![dynamic forms](./assets/image-2.png)
+
+- #### Document Editor 
+
+    ![doc_editor](./assets/image-4.png)
+
+- #### Downloading the document
+
+    ![download](./assets/image-3.png)
+
+- #### Chatbot 
+
+    ![chatbot](./assets/image-6.png)
+
+- #### FAQ page
+
+    ![faq](./assets/image-7.png)
+
+- #### About Page
+
+    ![about_page](./assets/image-5.png)
+
+## Contributors
+
+- [Shardul Khade](https://github.com/shark-21)
+- [Mihir Rathod](https://github.com/m-g-rathod)
+- [Prit Kanadiya](https://github.com/PritK99)
+- [Devayani Chandane](https://github.com/devayani03)
+- [Kavan Gandhi](https://github.com/KGan31)
+- [Vedant Nimje](https://github.com/vrnimje)
+
+## References
+
+- [LawRato](https://lawrato.com/legal-documents), for the dataset of legal documents
+- [Botpress](https://botpress.com/), for building the AI-powered chatbot
+
+
+
+
+
+
+
---
+++ Server/app.py
@@ -0,0 +1,199 @@
+from flask import Flask, request, jsonify, send_file
+import requests
+import MySQLdb
+from flask_cors import CORS
+from docx import Document
+import mammoth
+import psycopg2
+from dotenv import load_dotenv
+import os   
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from model.bot import get_response
+
+# Load environment variables from .env file
+load_dotenv()
+
+app = Flask(__name__)
+
+# Configure CORS to allow requests from React frontend
+cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+
+# db = MySQLdb.Connect(host="127.0.0.1", port=3306,
+#                      user="root", passwd="Utkarsh@123", db="leagal")
+db = psycopg2.connect(database=os.getenv('DATABASE_NAME'), user=os.getenv('DATABASE_USER'),
+                        password=os.getenv('PASSWORD'), host=os.getenv('DATABASE_HOST'), port='5432', keepalives=1, keepalives_idle=30,
+                        keepalives_interval=10, keepalives_count=5)
+
+# # Get all the services
+
+
+@app.route('/api/services', methods=["GET"])
+def services():
+    cur = db.cursor()
+    cur.execute('SELECT * FROM services')
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    cur.close()
+    print(json_data)
+    return jsonify(json_data)
+
+# Get forms of a particular service
+
+
+@app.route('/api/forms', methods=["GET"])
+def get_forms():
+    # Send json object {"service_id": "..."}
+    Service = request.args.get('service_id')
+    print(type(Service))
+    print(Service)
+    cur = db.cursor()
+    cur.execute(
+        "SELECT services.service_id, services.service_name, forms.form_id, forms.form_name, forms.form_link FROM services INNER JOIN forms ON services.service_id = forms.service_id WHERE forms.service_id = %s;", [Service])
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    cur.close()
+    print(json_data)
+    return jsonify(json_data)
+
+# Get all queries for a form
+
+
+@app.route('/api/form-details', methods=["GET"])
+def get_form_details():
+    # Send json object {"form_id":"..."}
+    form_id = request.args.get('form_id')
+    print(form_id)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM forms WHERE form_id = %s;", [form_id])
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    
+    cur.execute("SELECT * FROM ques_categories WHERE id IN (SELECT DISTINCT(category_id) FROM input_ques WHERE ques_id IN (SELECT form_query_id FROM form_queries WHERE form_id = %s));", [form_id])
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    cur.execute("SELECT * FROM input_ques WHERE ques_id IN (SELECT form_query_id FROM form_queries WHERE form_id = %s);", [form_id])
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    cur.close()
+    return jsonify(json_data)
+
+
+# Return the contents of final doc
+@app.route('/api/final-content', methods=["POST"])
+def final_content():
+    form_details = request.json                         # Under Progress
+    form_id = form_details["form_id"]
+    # print(type(form_details))
+    print(form_id)
+    cur = db.cursor()
+    cur.execute("SELECT form_link FROM forms where form_id = %s;", [form_id])
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    cur.close()
+    print(json_data[0]["form_link"])
+    response = requests.get(json_data[0]["form_link"])
+    with open('./docs/localfile.docx', 'wb') as f:
+        f.write(response.content)
+        
+    doc = Document('./docs/localfile.docx')
+    test = list([int(x) for x in form_details.keys() if x.isdigit()])
+    
+    test.sort(reverse=True)
+    print(test)
+    for key in test:
+        old = '#'+str(key)
+        new = str(form_details[str(key)])
+        
+        for p in doc.paragraphs:
+            if old in p.text:
+                # print(old)
+                inline = p.runs
+                for i in range(len(inline)):
+                    if old in inline[i].text:
+                        # print(old)
+                        res = inline[i].text.replace(old, new)
+                        inline[i].text = res
+    doc.save("./docs/Output2.docx")
+    
+    f = open('./docs/Output2.docx', 'rb')
+    
+    docx_content = mammoth.convert_to_html(f)
+    # print(docx_content.value)
+    # docx_content.close()
+
+    # fullText = []
+    # for para in doc.paragraphs:
+    #     fullText.append(para.text)
+    # fullText = '\n'.join(fullText)
+    # print(fullText)
+    return jsonify({'content': docx_content.value})
+ 
+# Return the final doc
+
+
+@app.route('/api/final-form', methods=["POST"])
+def final_form():
+    contents = request.get_json()
+    print(contents)
+    with open('docs/Output2.docx', 'w') as file:
+        file.write(contents)
+    # doc = Document('docs/localfile.docx')
+    # for key, value in form_details.items():
+    #     for paragraph in doc.paragraphs:
+    #         paragraph.text = paragraph.text.replace(
+    #             "#"+str(key)+'#', str(value))
+
+    # doc.save("docs/Output2.docx")
+    return send_file('./docs/Output2.docx', as_attachment=True)
+
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    user_input = request.json
+    
+    # sentence = user_input.user_chat
+
+    # sentence = tokenize(sentence)
+    # X = bag_of_words(sentence, words)
+    # X = X.reshape(1, X.shape[0])
+    # X = torch.from_numpy(X).to(device)
+
+    # output = model(X)
+    # _, predicted = torch.max(output, dim=1)
+
+    # tag = tags[predicted.item()]
+
+    # probs = torch.softmax(output, dim=1)
+    # prob = probs[0][predicted.item()]
+    # if prob.item() > 0.75:
+    #     for intent in intents['intents']:
+    #         if tag == intent["tag"]:
+    #             # print(f"{bot_name}: {random.choice(intent['responses'])}")
+    #             return jsonify({'aiMessage': random.choice(intent['responses'])})
+    # else:
+    #     # print(f"{bot_name}: I am unable to process the given query. Please try again")
+    #     return jsonify({'aiMessage': 'I am unable to process the given query. Please try again'})
+    return jsonify({'aiMessage': 'hello'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
